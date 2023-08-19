d3bd94e7b0c3479c33ed442c42422fa336cf0014
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-08-19 18:26:51 +0300

    инициализация проекта, конфиги

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './App.module.scss';
+import gsap from 'gsap';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      hello
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+     }
+    
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
